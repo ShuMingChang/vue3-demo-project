49e2ab2d06b10f9992f63643301ad56d46393526
Terry614 <minmin614@gmail.com>
2024-04-02 17:50:05 +0800

    add getAllConfig

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
-import { getRemoteConfig } from 'firebase/remote-config'
+import { fetchConfig, getRemoteConfig } from 'firebase/remote-config'
 import { worker } from './mocks/browser'
 
 import App from './App.vue'
@@ -38,7 +38,8 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig)
 const analytics = getAnalytics(firebaseApp)
 const remoteConfig = getRemoteConfig(firebaseApp)
-console.log(remoteConfig)
+const allConfig = await fetchConfig(remoteConfig)
+console.log(allConfig)
 const app = createApp(App)
 
 app.use(createPinia())
